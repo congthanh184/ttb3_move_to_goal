4a49991305467bd6f90e3a2f8dd5964ec0a51bfe
congthanh <congthanh184@gmail.com>
2019-09-09 00:16:04 +0800

    Correct order of transformation matrix = trans * rot

--- nodes/move_node.py
+++ nodes/move_node.py
@@ -38,16 +38,18 @@ if __name__ == '__main__':
 
                 translation = trans.transform.translation
                 quaternion = trans.transform.rotation
-                T1 = numpy.dot(tf.transformations.quaternion_matrix((quaternion.x, quaternion.y, quaternion.z, quaternion.w)),
-                               tf.transformations.translation_matrix((translation.x, translation.y, translation.z)))
+                print(translation)
+                print(quaternion)
+                T1 = numpy.dot(tf.transformations.translation_matrix((translation.x, translation.y, translation.z)),
+                               tf.transformations.quaternion_matrix((quaternion.x, quaternion.y, quaternion.z, quaternion.w)))
 
                 q2 = tf.transformations.quaternion_from_euler(float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
-                T2 = numpy.dot(tf.transformations.quaternion_matrix(q2),
-                           tf.transformations.translation_matrix((float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))))
+                T2 = numpy.dot(tf.transformations.translation_matrix((float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))), tf.transformations.quaternion_matrix(q2))
                 T_goal = numpy.dot(T1, T2)
+                print(q2)
                 print(T1)
-                print(T2)
-                print(T_goal)
+                #  print(T2)
+                #  print(T_goal)
 
                 object_transform = geometry_msgs.msg.TransformStamped()
                 object_transform.header.stamp = rospy.Time.now()
@@ -55,7 +57,7 @@ if __name__ == '__main__':
                 object_transform.child_frame_id = "goal"
                 object_transform = get_msg_from_matrix(object_transform, T_goal)
                 br.sendTransform(object_transform)
-                rospy.sleep(0.1)
+                rospy.sleep(5)
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as err:
                 print("Error", err)
 
