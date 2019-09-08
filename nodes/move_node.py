ac4021942ff3f85478f4927b1ed64fdf16050678
congthanh <congthanh184@gmail.com>
2019-09-08 23:35:37 +0800

    Publish Goal Transform continuously

--- nodes/move_node.py
+++ nodes/move_node.py
@@ -32,37 +32,30 @@ if __name__ == '__main__':
         br = tf2_ros.TransformBroadcaster()
         rospy.sleep(0.5)
 
-        try:
-            trans = tfBuffer.lookup_transform("odom", "base_footprint", rospy.Time(0), rospy.Duration(5.0))
+        while not rospy.is_shutdown():
+            try:
+                trans = tfBuffer.lookup_transform("odom", "base_footprint", rospy.Time(0), rospy.Duration(5.0))
 
-            translation = trans.transform.translation
-            quaternion = trans.transform.rotation
-            T1 = numpy.dot(tf.transformations.quaternion_matrix((quaternion.x, quaternion.y, quaternion.z, quaternion.w)),
-                           tf.transformations.translation_matrix((translation.x, translation.y, translation.z)))
+                translation = trans.transform.translation
+                quaternion = trans.transform.rotation
+                T1 = numpy.dot(tf.transformations.quaternion_matrix((quaternion.x, quaternion.y, quaternion.z, quaternion.w)),
+                               tf.transformations.translation_matrix((translation.x, translation.y, translation.z)))
 
-            print sys.argv
-            q2 = tf.transformations.quaternion_from_euler(float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
-            T2 = numpy.dot(tf.transformations.quaternion_matrix(q2),
+                q2 = tf.transformations.quaternion_from_euler(float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
+                T2 = numpy.dot(tf.transformations.quaternion_matrix(q2),
                            tf.transformations.translation_matrix((float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))))
-            T_goal = numpy.dot(T1, T2)
-            print(T1)
-            print(T2)
-            print(T_goal)
+                T_goal = numpy.dot(T1, T2)
+                print(T1)
+                print(T2)
+                print(T_goal)
 
-            object_transform = geometry_msgs.msg.TransformStamped()
-            object_transform.header.stamp = rospy.Time.now()
-            object_transform.header.frame_id = "odom"
-            object_transform.child_frame_id = "goal"
-            object_transform = get_msg_from_matrix(object_transform, T_goal)
-            while not rospy.is_shutdown():
+                object_transform = geometry_msgs.msg.TransformStamped()
+                object_transform.header.stamp = rospy.Time.now()
+                object_transform.header.frame_id = "odom"
+                object_transform.child_frame_id = "goal"
+                object_transform = get_msg_from_matrix(object_transform, T_goal)
                 br.sendTransform(object_transform)
-                rospy.sleep(0.05)
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as err:
-            print("Error", err)
-
-        sys.exit(0)
-
-
-
-
+                rospy.sleep(0.1)
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as err:
+                print("Error", err)
 
